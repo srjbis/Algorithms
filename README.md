b2f0ef362da33ebc42545c0eecf299d9078d37e9
Abhijit Maity <abhijit@plank.life>
2016-08-24 14:25:43 +0530

    #2 Selection sort

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Algorithms
+
+## $ Sorting and searching
+
+1. Binary search
+2. Binary search with recursion
+3. Bobble sort
+4. Optimized bobble sort
+5. Selection sort
---
+++ selection_sort.py
@@ -0,0 +1,21 @@
+"""
+Selection sort
+Complexity:
+"""
+
+
+def selection_sort(list_of_items):
+    for index in range(len(list_of_items) - 1, 0, -1):
+        max_position = 0
+        for location in range(1, index + 1):
+            if list_of_items[location] > list_of_items[max_position]:
+                max_position = location
+        temp = list_of_items[index]
+        list_of_items[index] = list_of_items[max_position]
+        list_of_items[max_position] = temp
+
+if __name__ == '__main__':
+    test_list = [23, 34, 10, 4, 333, 452]
+    print 'Before sort: ', test_list
+    selection_sort(test_list)
+    print 'After sort: ', test_list
